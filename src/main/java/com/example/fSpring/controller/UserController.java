2d34db36744f5db07b462a0d0a8e81fabf1fbbf4
fedia233 <cholak2009@mail.ru>
2018-08-21 22:32:45 +0300

    change role

---
+++ src/main/java/com/example/fSpring/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.fSpring.controller;
+
+
+import com.example.fSpring.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+
+        return "userList";
+    }
+}
